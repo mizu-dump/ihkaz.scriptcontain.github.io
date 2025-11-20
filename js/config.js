8a85105578ed90dd6c381b7bb09dec031002270e
𝐌 𝐈 𝐙 𝐔 𝐌 𝐀 𝐑 𝐔 <mizumaruofficialdump@gmail.com>
2025-11-20 10:31:54 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,8 +6,6 @@ window.scriptData = [
     description: "Script Spawner, Auto For Grow A Garden",
     category: "Helper",
     tags: ["helper", "gag", "roblox", "spawner"],
-    image: "https://tr.rbxcdn.com/180DAY-91f8ec73480a974786806c66d92dd30e/768/432/Image/Webp/noFilter
-      
-    "
+    image: "https://tr.rbxcdn.com/180DAY-91f8ec73480a974786806c66d92dd30e/768/432/Image/Webp/noFilter"
   }
 ];
