d0a9e039d9dc63cc32ad47d68808d76b6cd85132
𝐌 𝐈 𝐙 𝐔 𝐌 𝐀 𝐑 𝐔 <mizumaruofficialdump@gmail.com>
2025-11-20 10:58:51 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,8 +4,48 @@ window.scriptData = [
     title: "MZZHUB [Grow A Garden]",
     url: "https://pastebin.com/raw/6p5TTn3e",
     description: "Script Spawner, Auto For Grow A Garden",
-    category: "Helper",
+    category: "Spawner",
     tags: ["helper", "gag", "roblox", "spawner"],
     image: "https://tr.rbxcdn.com/180DAY-91f8ec73480a974786806c66d92dd30e/768/432/Image/Webp/noFilter"
+  },
+
+  {
+    id: "sab_script",
+    title: "MZZHUB [Steal A Brainrot]",
+    url: "https://pastebin.com/raw/6p5TTn3e",
+    description: "Script Brainrot Sniper, ESP, Desync For Steal A Brainrot",
+    category: "Helper",
+    tags: ["sab", "autosteal", "esp", "desync"],
+    image: "https://tr.rbxcdn.com/180DAY-b6ad9f8a2edddee0365eed4f19a8caf9/768/432/Image/Webp/noFilter"
+  },
+
+  {
+    id: "99_script",
+    title: "MZZHUB [99 Nights In The Forest]",
+    url: "https://pastebin.com/raw/6p5TTn3e", // change to yours
+    description: "Script Mob ESP, Auto Farm Days, Diamonds, Bring For 99 Nights In The Forest",
+    category: "Survival",
+    tags: ["forest", "night", "survival", "esp"],
+    image: "https://tr.rbxcdn.com/180DAY-643b9b9285f3bd9b63cf392db12a4d87/768/432/Image/Webp/noFilter"
+  },
+
+  {
+    id: "pvb_script",
+    title: "MZZHUB [Plants Vs Brainrots]",
+    url: "https://pastebin.com/raw/6p5TTn3e",
+    description: "Script Spawner For Plants Vs Brainrots",
+    category: "Spawner",
+    tags: ["pvb", "plants", "brainrot", "spawner"],
+    image: "https://tr.rbxcdn.com/180DAY-a933a9ba765efea4061f4a101f18edd2/768/432/Image/Webp/noFilter"
+  },
+
+  {
+    id: "slap_script",
+    title: "MZZHUB [Slap Tower]",
+    url: "https://pastebin.com/raw/6p5TTn3e",
+    description: "Script Auto Slap, Anti Void, Slap Aura, Unlock Gamepass For Slap Tower",
+    category: "Combat",
+    tags: ["slap", "gamepass", "troll", "slapaura"],
+    image: "https://tr.rbxcdn.com/180DAY-d02a1823db10e3dd0357dd1ba10f53d8/768/432/Image/Webp/noFilter"
   }
 ];
